a7b043e77bd218cd4d3fd750aa6db82b89391693
smita gule <smita20gule@gmail.com>
2020-12-09 12:46:14 +0530

    initialized variables

---
+++ initialize.m
@@ -0,0 +1,16 @@
+%Initial values of parameters in the mass-spring-damper system
+
+%Mass M1
+M1 = 100;
+
+%Spring k1 constant
+k1 = 50;
+
+%Damping co-efficient
+b = 50;
+
+%Mass M2
+M2 = 50;
+
+%Spring k12 constant
+k12 = 25;
